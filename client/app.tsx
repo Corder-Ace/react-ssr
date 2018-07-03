13f6918154a8717afba410c09fbe3e3353b2ec95
yangziqi <yangziqi@vackbot.com>
2018-07-03 18:08:15 +0800

    测试ts

--- client/app.tsx
+++ client/app.tsx
@@ -1,23 +1,23 @@
-import * as React from 'react'
+import * as React from 'react';
 import { Button } from 'antd';
 
 export interface IState {
-    btn_desc: string;
+	btn_desc: string;
 }
 class App extends React.Component<{}, IState> {
-    state = {
-        btn_desc: '1234567'
-    }
+	state = {
+		btn_desc: '1234567'
+	};
 
-    render() {
-        const { btn_desc } = this.state
-        return (
-            <div>
-                <Button type="primary">{btn_desc}</Button>
-                {/* <h1>123213</h1> */}
-            </div>
-        )
-    }
+	render() {
+		const { btn_desc } = this.state;
+		return (
+			<div>
+				<Button type="primary">{btn_desc}</Button>
+				{/* <h1>123213</h1> */}
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
